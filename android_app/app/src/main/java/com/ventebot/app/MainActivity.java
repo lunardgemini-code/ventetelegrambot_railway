025de54f7386066c1f67facd7f054e814cc69bf8
lunardgemini-code <lunardgemini@gmail.com>
2026-06-25 13:46:44 +0100

    Add files via upload

--- android_app/app/src/main/java/com/ventebot/app/MainActivity.java
+++ android_app/app/src/main/java/com/ventebot/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.ventebot.app;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -25,9 +27,19 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         
         // Ensure links open inside the WebView instead of external browser
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @SuppressLint("WebViewClientOnReceivedSslError")
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                // Ignore les erreurs de certificat SSL (très courant sur d'anciens téléphones Android avec Let's Encrypt)
+                handler.proceed(); 
+            }
+        });
         webView.loadUrl(TARGET_URL);
     }
 
