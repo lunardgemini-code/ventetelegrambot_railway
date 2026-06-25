c83db79ded81af14d30c47aad3e7f922212c108f
lunardgemini-code <lunardgemini@gmail.com>
2026-06-25 15:16:25 +0100

    Add files via upload

--- android_app/app/src/main/java/com/ventebot/app/MainActivity.java
+++ android_app/app/src/main/java/com/ventebot/app/MainActivity.java
@@ -3,7 +3,9 @@ package com.ventebot.app;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.net.http.SslError;
+import android.webkit.CookieManager;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -24,19 +26,29 @@ public class MainActivity extends AppCompatActivity {
         webView = new WebView(this);
         setContentView(webView);
 
+        // Enable cookies (required for cross-origin fetch)
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setAllowContentAccess(true);
         webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setUserAgentString(webSettings.getUserAgentString().replace("; wv", ""));
         
+        // Enable console & JS dialogs
+        webView.setWebChromeClient(new WebChromeClient());
+
         // Ensure links open inside the WebView instead of external browser
         webView.setWebViewClient(new WebViewClient() {
             @SuppressLint("WebViewClientOnReceivedSslError")
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                // Ignore les erreurs de certificat SSL (très courant sur d'anciens téléphones Android avec Let's Encrypt)
                 handler.proceed(); 
             }
         });
